8b1c62ff3e374db3ca019d0727e0c82d0a88aeb6
Aleksei Vlasov <alekseivlasov@Alekseis-Mac-mini.local>
2024-12-04 21:56:03 +0800

    hero animation is done

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -3,15 +3,24 @@ import { Routes, Route } from 'react-router-dom';
 import HeroSection from './components/HeroSection/Hero';
 import LearnMorePage from './components/LearnMore/LearnMore';
 import StartJourneyPage from './components/StartJourney/StartJourney';
+import Products from './components/Products/Products';
 import './App.css';
+
 
 function App() {
     return (
+        <>
+        <HeroSection />
+        
+      
+        <Products />
+
+    
         <Routes>
-            <Route path="/" element={<HeroSection />} />
             <Route path="/learn-more" element={<LearnMorePage />} />
             <Route path="/start-journey" element={<StartJourneyPage />} />
         </Routes>
+    </>
     );
 }
 
--- my-app/src/components/HeroSection/Hero.jsx
+++ my-app/src/components/HeroSection/Hero.jsx
@@ -1,33 +1,17 @@
 import React from 'react';
 import styles from './Hero.module.css';
+import StarCanvas from '../StarCanvas/StarCanvas'
 
 const HeroSection = () => {
     return (
-        // <div className={styles.heroSection}>
-        //     <div className={styles.splitScreen}>
-        //         <div className={styles.left}></div>
-        //         <div className={styles.right}></div>
-        //     </div>
-        //     <div className={styles.heroContent}>
-        //         <h1>Unlock the Power of Your Mind and Body</h1>
-        //         <p>Discover the science of enhancement with Семакс, Фенотропил, and ИРС19. Explore how these innovations can sharpen your focus, boost your energy, and enhance your well-being.</p>
-        //         <div className={styles.ctaButtons}>
-        //             <button className={`${styles.ctaButton} ${styles.learnMore}`}>Learn More</button>
-        //             <button className={`${styles.ctaButton} ${styles.startJourney}`}>Start Your Enhancement Journey</button>
-        //         </div>
-        //         <p className={styles.tagline}>Evolve Your Potential. Backed by scientific research and used by those who aim to achieve more.</p>
-        //     </div>
-        // </div>
         <div className={styles.container}>
-        {/* Hero Section */}
         <div className={styles.hero}>
           <div className={styles.heroContent}>
             <h1>Unlock Your Potential</h1>
             <p>Empower your mind with clarity, focus, and boundless creativity.</p>
+           
           </div>
-  
-          {/* Product Section Fixed at the Bottom */}
-          <div className={styles.products}>
+          {/* <div className={styles.products}>
             <div className={styles.product}>
               <img src="images/f.jpg" alt="Fenotropil" />
               <h2>Fenotropil</h2>
@@ -48,8 +32,9 @@ const HeroSection = () => {
               <h2>Noopept</h2>
               <p>Boost cognition and protect your brain.</p>
             </div>
-          </div>
+          </div> */}
         </div>
+        <StarCanvas />
       </div>
     );
 };
