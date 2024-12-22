d6c5008516b07eb29caa37e09c877ceb82584f19
Aleksei Vlasov <alekseivlasov@Alekseis-Mac-mini.local>
2024-12-23 00:13:32 +0800

    navbar added current location

--- my-app-vite/src/components/Navbar/Navbar.jsx
+++ my-app-vite/src/components/Navbar/Navbar.jsx
@@ -1,26 +1,75 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 import { useTranslation } from 'react-i18next';
+import { Link, useLocation } from 'react-router-dom';
+import { Menu } from 'antd';
 import styles from './Navbar.module.css';
 
 export default function Navbar() {
   const { t } = useTranslation();
+  const location = useLocation();
+
+  // Define items so that key === route pathname
+  const items = [
+    {
+      key: '/products',
+      label: (
+        <Link className={styles.link} to="/products">
+          {t('navbar.products')}
+        </Link>
+      ),
+    },
+    {
+      key: '/about',
+      label: (
+        <Link className={styles.link} to="/about">
+          {t('navbar.about')}
+        </Link>
+      ),
+    },
+    {
+      key: '/order',
+      label: (
+        <Link className={styles.link} to="/order">
+          {t('navbar.how_to_order')}
+        </Link>
+      ),
+    },
+    {
+      key: '/blogs',
+      label: (
+        <Link className={styles.link} to="/blogs">
+          {t('navbar.blog')}
+        </Link>
+      ),
+    },
+    {
+      key: '/faq',
+      label: (
+        <Link className={styles.link} to="/faq">
+          {t('navbar.faq')}
+        </Link>
+      ),
+    },
+  ];
+
+  // If the current path is one of the item keys, highlight it
+  // Otherwise, highlight nothing
+  const isValidPath = items.some((item) => item.key === location.pathname);
+  const selectedKeys = isValidPath ? [location.pathname] : [];
 
   return (
     <div className={styles.container}>
       <div className={styles.wrapper}>
-        <Link to='/'>
+        <Link to="/">
           <img className={styles.logo} src="../logo.png" alt="Logo" />
         </Link>
-        <nav className={styles.navbar}>
-          <Link className={styles.link} to='/products'>{t('navbar.products')}</Link>
-          <Link className={styles.link} to='/about'>{t('navbar.about')}</Link>
-          <Link className={styles.link} to='/order'>{t('navbar.how_to_order')}</Link>
-          <Link className={styles.link} to='/blogs'>{t('navbar.blog')}</Link>
-          <Link className={styles.link} to='/faq'>{t('navbar.faq')}</Link>
-        </nav>
+        <Menu
+          selectedKeys={selectedKeys}
+          mode="horizontal"
+          items={items}
+          className={styles.navbar}
+        />
       </div>
     </div>
   );
 }
-
